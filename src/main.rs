dbe2d4f7399dbad735b888108a2eecc9cc2c9a63
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-04-15 00:13:26 +0600

    impl type

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,86 @@
-use arrow_convert::{deserialize::TryIntoCollection, serialize::TryIntoArrow, ArrowField, ArrowSerialize, ArrowDeserialize};
-use serde::{Serialize, Deserialize, Deserializer, Serializer};
-use serde::de::{self, Visitor};
-use std::fmt;
+use arrow::array::{FixedSizeBinaryArray, builder};
 use arrow::datatypes::DataType;
-use arrow::array::{builder, FixedSizeBinaryArray};
 use arrow_convert::deserialize::ArrowArrayIterable;
+use arrow_convert::{
+    ArrowDeserialize, ArrowField, ArrowSerialize, deserialize::TryIntoCollection,
+    serialize::TryIntoArrow,
+};
+use serde::de::{self, Visitor};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt;
 
 fn main() {
-    println!("Hello, world!");
 }
+
+type Hash = [u8; 32];
+type Address = [u8; 20];
 
 #[derive(Serialize, Deserialize, ArrowField, ArrowSerialize, ArrowDeserialize)]
 struct Transaction {
+    #[serde(serialize_with = "serialize_hash")]
+    #[serde(deserialize_with = "deserialize_hash")]
     block_hash: Hash,
     block_number: u64,
+    #[serde(serialize_with = "serialize_address")]
+    #[serde(deserialize_with = "deserialize_address")]
     from: Address,
+    #[serde(serialize_with = "serialize_hash")]
+    #[serde(deserialize_with = "deserialize_hash")]
     hash: Hash,
+    #[serde(serialize_with = "serialize_binary")]
+    #[serde(deserialize_with = "deserialize_binary")]
+    input: Vec<u8>,
+    #[serde(serialize_with = "serialize_address")]
+    #[serde(deserialize_with = "deserialize_address")]
+    to: Address,
+    transaction_index: u64,
 }
 
-type Hash = FixedSizeData<32>;
-type Address = FixedSizeData<20>;
-
-pub struct FixedSizeData<const N: usize>(Box<[u8; N]>);
-
-impl<const N: usize> FixedSizeData<N> {
-    fn as_slice(&self) -> &[u8] {
-        &*self.0
-    }
+fn serialize_address<S>(x: &Address, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    s.serialize_str(&encode_hex(x.as_slice()))
 }
 
-impl<const N: usize> From<&[u8]> for FixedSizeData<N> {
-    fn from(buf: &[u8]) -> FixedSizeData<N> {
-        let buf: [u8; N] = buf.try_into().unwrap();
-        FixedSizeData(Box::new(buf))
-    }
+fn serialize_hash<S>(x: &Hash, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    s.serialize_str(&encode_hex(x.as_slice()))
 }
 
-struct FixedSizeDataVisitor<const N: usize>;
-
-impl<'de, const N: usize> Visitor<'de> for FixedSizeDataVisitor<N> {
-    type Value = FixedSizeData<N>;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str(&format!("hex string for {N} byte data"))
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        let buf = decode_hex(value);
-
-        Ok(Self::Value::from(buf.as_slice()))
-    }
+fn serialize_binary<S>(x: &Vec<u8>, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    s.serialize_str(&encode_hex(x.as_slice()))
 }
 
-impl<'de, const N: usize> Deserialize<'de> for FixedSizeData<N> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(FixedSizeDataVisitor)
-    }
+fn deserialize_address<'de, D>(d: D) -> Result<Address, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = String::deserialize(d)?;
+    let buf = decode_hex(value.as_str());
+    Ok(buf.as_slice().try_into().unwrap())
 }
 
-impl<const N: usize> Serialize for FixedSizeData<N> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&encode_hex(self.as_slice()))
-    }
+fn deserialize_hash<'de, D>(d: D) -> Result<Hash, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = String::deserialize(d)?;
+    let buf = decode_hex(&value);
+    Ok(buf.as_slice().try_into().unwrap())
+}
+
+fn deserialize_binary<'de, D>(d: D) -> Result<Vec<u8>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = String::deserialize(d)?;
+    Ok(decode_hex(&value))
 }
 
 fn encode_hex(buf: &[u8]) -> String {
@@ -78,43 +88,12 @@ fn encode_hex(buf: &[u8]) -> String {
 }
 
 fn decode_hex(value: &str) -> Vec<u8> {
-    let hex = value
-        .strip_prefix("0x")
-        .unwrap();
+    let hex = value.strip_prefix("0x").unwrap();
 
-    let len = hex.as_bytes().len();
+    let len = hex.len();
     let mut dst = vec![0; len / 2];
 
     faster_hex::hex_decode(hex.as_bytes(), &mut dst).unwrap();
 
     dst
 }
-
-impl<const N: usize> arrow_convert::field::ArrowField for FixedSizeData<N> {
-    type Type = Self;
-
-    fn data_type() -> DataType {
-        return DataType::FixedSizeBinary(i32::try_from(N).unwrap())
-    }
-}
-
-impl<const N: usize> arrow_convert::serialize::ArrowSerialize for FixedSizeData<N> {
-    type ArrayBuilderType = builder::FixedSizeBinaryBuilder;
-
-    fn new_array() -> Self::ArrayBuilderType {
-        Self::ArrayBuilderType::new(i32::try_from(N).unwrap())
-    }
-
-    fn arrow_serialize(v: &Self, array: &mut Self::ArrayBuilderType) -> arrow::error::Result<()> {
-        todo!()
-    }
-}
-
-impl<const N: usize> arrow_convert::deserialize::ArrowDeserialize for FixedSizeData<N> {
-    type ArrayType = FixedSizeBinaryArray;
-
-    fn arrow_deserialize(v: <Self::ArrayType as ArrowArrayIterable>::Item<'_>) -> Option<Self> {
-        todo!()
-    }
-}
-
